66ccf5f6a2d2b1f69e6c3abdc1164ef18346506c
benjaminsunderland <ben@sunderland.eu.com>
2018-01-20 21:18:16 +0000

    Added public folder with styling in speakers routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+
 var router = express.Router();
 
 router.get('/', (req, res) => {
   res.send(`
+      <link rel="stylesheet" type="text/css" href="css/style.css">
       <h1>Welcome</h1>
+      <img src="/images/misc/background.jpg" alt="background" style="height: 300px;">
   `);
 });
 
--- app/routes/speakers.js
+++ app/routes/speakers.js
@@ -9,11 +9,16 @@ router.get('/speakers', (req, res) => {
     info += `
     <li>
       <h2>${item.name}</h2>
+       <img src="/images/speakers/${item.shortname}_tn.jpg" alt="speaker"
       <p>${item.summary}</p>
     </li>
     `;
   })
   res.send(`
+      <link rel="stylesheet" type="text/css" href="/css/style.css">
+      <h1>Welcome</h1>
+      <img src="/images/misc/background.jpg" alt="background" style="height: 300px;">
+
       <h1>Meetups</h1>
       ${info}
     `)
@@ -24,9 +29,13 @@ router.get('/speakers/:speakerid', (req, res) => {
  var dataFile = req.app.get('appData')
  var speaker = dataFile.speakers[req.params.speakerid];
  res.send(`
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
     <h1>Your Speaker Requested Is:</h1>
     <h2>${speaker.title}</h2>
     <h3>${speaker.name}</h3>
+    <img src="/images/speakers/${speaker.shortname}_tn.jpg" alt="speaker"
     <p>${speaker.summary}</p>
   `)
 })
+
+module.exports = router;
