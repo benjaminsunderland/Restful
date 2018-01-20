354a2e3306d53edee9c97634f4d0135036ca457d
benjaminsunderland <ben@sunderland.eu.com>
2018-01-20 19:57:51 +0000

    Port now set at 3000

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+var dataFile = require('./data/data.json');
+app.set('port', process.env.PORT || 3000 );
 
 app.get('/', (req, res) => {
   res.send("<li>This is working!</li>")
 })
-app.listen(3000, () => {
-  console.log("Listening, baby!")
+app.listen(app.get('port'), () => {
+  console.log('Listening on port ' + app.get('port'));
 });
