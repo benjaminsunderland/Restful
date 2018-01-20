a968fc3fa59d9187f780df153595e3eaeebcf484
benjaminsunderland <ben@sunderland.eu.com>
2018-01-20 20:02:30 +0000

    Added data from json file to res.send

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ var dataFile = require('./data/data.json');
 app.set('port', process.env.PORT || 3000 );
 
 app.get('/', (req, res) => {
-  res.send("<li>This is working!</li>")
+  var info = '';
+  dataFile.speakers.forEach(function(item) {
+    info += `
+    <li>
+      <h2>${item.name}</h2>
+      <p>${item.summary}</p>
+    </li>
+    `;
+  })
+  res.send(`
+      <h1>Meetups</h1>
+      ${info}
+    `)
 })
 app.listen(app.get('port'), () => {
   console.log('Listening on port ' + app.get('port'));
